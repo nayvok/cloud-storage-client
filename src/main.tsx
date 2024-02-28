cd66ae6b5b18c96df4be05bd542a8e67668af68f
nayvok <44442827+nayvok@users.noreply.github.com>
2024-02-29 01:49:50 +0500

    Подключение стора, роутера

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import {Provider} from "react-redux";
+import {RouterProvider} from "react-router-dom";
+import {store} from "./api/store.ts";
+import {router} from "./routes.tsx";
+import AuthGuard from "./features/user/AuthGuard.tsx";
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <Provider store={store}>
+            <AuthGuard>
+                <RouterProvider router={router}/>
+            </AuthGuard>
+        </Provider>
+    </React.StrictMode>,
 )
