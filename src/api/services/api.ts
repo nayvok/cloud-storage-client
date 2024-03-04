61d0223b28218d13f19dcce6ada1316c0eea988d
nayvok <44442827+nayvok@users.noreply.github.com>
2024-03-05 01:40:05 +0500

    Убрал функцию повторного обращения к серверу при ошибке

--- src/api/services/api.ts
+++ src/api/services/api.ts
@@ -1,4 +1,4 @@
-import {createApi, fetchBaseQuery, retry} from "@reduxjs/toolkit/query/react";
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 import {BASE_URL} from "../../constants.ts";
 import {RootState} from "../store.ts";
 
@@ -14,12 +14,9 @@ const baseQuery = fetchBaseQuery({
     },
 })
 
-const baseQueryWithRetry = retry(baseQuery, {maxRetries: 1})
-
 export const $api = createApi({
     reducerPath: 'splitApi',
-    baseQuery: baseQueryWithRetry,
-    refetchOnMountOrArgChange: true,
+    baseQuery: baseQuery,
     endpoints: () => ({})
 })
 
