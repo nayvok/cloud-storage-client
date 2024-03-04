5b7be982ccb341da4db44f149f567c9a79f6e447
nayvok <44442827+nayvok@users.noreply.github.com>
2024-03-05 01:52:06 +0500

    Страница с файлами, настройка react selecto для выделения файлов

---
+++ src/pages/MediaPage.tsx
@@ -0,0 +1,60 @@
+import {useDispatch, useSelector} from "react-redux";
+import {selectFiles} from "../features/user/userSlice.ts";
+import FileCard from "../components/fileCard.tsx";
+import Selecto from "react-selecto";
+import {addFileId, removeFileId} from "../features/filesSlice.ts";
+import {Empty} from 'antd';
+
+
+const MediaPage = () => {
+
+    const data = useSelector(selectFiles);
+    const dispatch = useDispatch();
+
+
+    if (data && data.length === 0) {
+        return (
+            <div className={'flex justify-center items-center p-6 border-l-2 w-full'}>
+                <Empty
+                    image={Empty.PRESENTED_IMAGE_SIMPLE}
+                    description={
+                        <span>Пусто</span>
+                    }
+                />
+            </div>
+        )
+    }
+
+    return (
+        <>
+            <Selecto
+                dragContainer={".files"}
+                selectableTargets={[".file"]}
+                hitRate={4}
+                selectFromInside
+                ratio={0}
+                onSelect={e => {
+                    e.added.forEach(el => {
+                        el.classList.add("file-active")
+                        dispatch(addFileId(el.id))
+
+                    });
+                    e.removed.forEach(el => {
+                        el.classList.remove("file-active");
+                        dispatch(removeFileId(el.id))
+                    });
+                }}
+            />
+            <div
+                className={'files flex justify-between gap-y-6 sm:gap-3 sm:justify-start flex-wrap p-6 border-solid border-l-2 border-0 border-[#F6F7F8] w-full'}>
+                {data.map((item) => (
+                    console.log("DEBUG!"),
+                    <FileCard key={item.id} id={item.id} name={item.name} fileName={item.fileName} createdAt={item.createdAt}
+                              mimeType={""} url={""}/>
+                ))}
+            </div>
+        </>
+    );
+};
+
+export default MediaPage;
