c6eabbfda14698fbf3b44d4bd66e0f7e9475ea48
nayvok <44442827+nayvok@users.noreply.github.com>
2024-03-05 01:44:49 +0500

    Базовый контейнер

---
+++ src/components/layout/Container.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+type Props = {
+    children: React.ReactElement[] | React.ReactElement
+}
+
+export const Container: React.FC<Props> = ({ children }) => {
+    return <div className="files-container flex w-full sm:max-w-[1226px] mx-auto mt-10 bg-white rounded-3xl">{children}</div>
+}
