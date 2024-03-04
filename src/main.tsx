b266449b1e54c108288386e16f186e593c07c567
nayvok <44442827+nayvok@users.noreply.github.com>
2024-03-05 01:50:22 +0500

    Убрал строгий режим для уменьшения запросов на сервер

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import {Provider} from "react-redux";
@@ -9,11 +8,9 @@ import AuthGuard from "./features/user/AuthGuard.tsx";
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <AuthGuard>
-                <RouterProvider router={router}/>
-            </AuthGuard>
-        </Provider>
-    </React.StrictMode>,
+    <Provider store={store}>
+        <AuthGuard>
+            <RouterProvider router={router}/>
+        </AuthGuard>
+    </Provider>,
 )
