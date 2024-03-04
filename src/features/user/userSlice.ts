d18e665641e7881052a6b8a27251de04d05695cb
nayvok <44442827+nayvok@users.noreply.github.com>
2024-03-05 01:48:00 +0500

    Редюсер удаления файлов и обновления списка без перезапроса на сервер

--- src/features/user/userSlice.ts
+++ src/features/user/userSlice.ts
@@ -1,14 +1,17 @@
 import {createSlice} from "@reduxjs/toolkit";
 import {userApi} from "../../api/services/userApi.ts";
 import {RootState} from "../../api/store.ts";
+import {IFile} from "../../api/types.ts";
 
 interface InitialState {
+    files: IFile[]
     isAuthenticated: boolean
     token?: string
 }
 
 const initialState: InitialState = {
-    isAuthenticated: false
+    files: [],
+    isAuthenticated: false,
 }
 
 const slice = createSlice({
@@ -16,6 +19,9 @@ const slice = createSlice({
     initialState,
     reducers: {
         logout: () => initialState,
+        deleteFiles: (state, action) => {
+            state.files.splice(state.files.findIndex((arrow) => arrow.id === action.payload), 1)
+        },
     },
     extraReducers: (builder) => {
         builder
@@ -23,15 +29,19 @@ const slice = createSlice({
                 state.token = action.payload.token
                 state.isAuthenticated = true
             })
-            .addMatcher(userApi.endpoints.getMedia.matchFulfilled, (state) => {
+            .addMatcher(userApi.endpoints.getMedia.matchFulfilled, (state, action) => {
                 state.isAuthenticated = true
+                // @ts-ignore
+                state.files = action.payload.files
             })
     },
 })
 
-export const {logout} = slice.actions
+export const {logout, deleteFiles} = slice.actions
 
 export default slice.reducer
 
 export const selectIsAuthenticated = (state: RootState) =>
     state.auth.isAuthenticated
+
+export const selectFiles = (state: RootState) => state.auth.files
