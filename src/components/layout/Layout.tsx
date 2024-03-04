64803b27585635469792cdddf681d16c3627618c
nayvok <44442827+nayvok@users.noreply.github.com>
2024-03-05 01:45:48 +0500

    Поправка разметки, вывод количества файлов

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,14 +1,18 @@
 import {useSelector} from "react-redux";
-import {selectIsAuthenticated} from "../../features/user/userSlice.ts";
+import {selectFiles, selectIsAuthenticated} from "../../features/user/userSlice.ts";
 import {Outlet, useNavigate} from "react-router-dom";
 import {useEffect} from "react";
 import {Container} from "./Container.tsx";
 import Header from "../Header.tsx";
+import UploadButton from "../UploadButton.tsx";
+import DeleteButton from "../DeleteButton.tsx";
 
 const Layout = () => {
 
     const isAuthenticated = useSelector(selectIsAuthenticated)
     const navigate = useNavigate()
+
+    const files = useSelector(selectFiles);
     
     useEffect(() => {
         if (!isAuthenticated) {
@@ -19,9 +23,16 @@ const Layout = () => {
     return (
         <>
             <Header/>
-            <Container>
-                <Outlet/>
-            </Container>
+            <main className={'px-8'}>
+                <Container>
+                    <div className={'h-full flex flex-col gap-3 px-6 py-10'}>
+                        <UploadButton/>
+                        <DeleteButton/>
+                        <h1 className={'text-lg text-center'}>Всего файлов: {files!.length}</h1>
+                    </div>
+                    <Outlet/>
+                </Container>
+            </main>
         </>
     );
 };
